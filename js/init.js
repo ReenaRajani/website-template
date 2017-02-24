489e41230255b5c95974238c0e1429f33aa915c9
reena.rajani <reenarajani@Reenas-MBP.lan>
2017-02-24 22:11:01 +1100

    Website template

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
